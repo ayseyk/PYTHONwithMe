01003524cede992de3a8895c1e632a02e6b52da7
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2021-11-01 18:06:12 +0300

    Intro file was added.

---
+++ intro.py
@@ -0,0 +1,16 @@
+x=5
+y=6
+print(x+y)
+
+name = "Ayşemsi" # 0' dan 4. karaktere kadar, ikişer ikişer. --> Aşm
+print(name[0:5:2])
+
+name = "Ayşe"
+age = 21
+print("My name is {} {}".format(name,age))
+
+result = 200/700
+print('The result is {r}'.format(r = result))
+
+name = "AYŞE"
+print(name[::-1])
