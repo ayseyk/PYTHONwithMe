49d896b4abd2fbfb41f639f345c03fd3a58a943f
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2021-11-04 18:04:26 +0300

    List-exercises file was added.

---
+++ list-examples.py
@@ -0,0 +1,26 @@
+# text= '25'.isdigit()
+# print(text)
+
+# message="do something now. ı say for you."
+# result= message.replace(' ','*',2)
+# print(result)
+
+# userA=['ayşe',21]
+# userB=['şeyma',17]
+# users=[userA,userB]
+# result=users[0][1]
+
+#  mylist = [1,2,3,4,5]
+
+# result=mylist[0::2]
+# result=mylist[-2:]
+
+# result=(60+70+70)//3
+
+cars= ["ay","by","cy"]
+cars.insert(-1,"car")
+print(cars)
+
+print(len(cars))
+
+
